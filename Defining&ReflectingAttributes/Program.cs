c5b2cd33e546b91411bd1d8848807d3a3a3b9d51
Kulpot <kulpotwahu@gmail.com>
2023-04-10 21:21:51 +0800

    Add project files.

---
+++ Defining&ReflectingAttributes/Program.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace DefiningAndReflectingAttributes
+{
+    [Author("Kulpot Wahu", Age = 999)]
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type info = Type.GetType("DefiningAndReflectingAttributes.Program");
+
+            foreach(var attrib in info.CustomAttributes)
+            {
+                if(attrib.AttributeType == typeof(AuthorAttribute))
+                {
+                    Console.WriteLine(attrib.ConstructorArguments[0].Value.ToString());
+                    
+                    if(attrib.NamedArguments.Count > 0)
+                    {
+                        int age = (int)attrib.NamedArguments[0].TypedValue.Value;
+                        Console.WriteLine(age.ToString());
+                    }
+                }
+            }
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    class AuthorAttribute : Attribute
+    {
+        public int Age;
+
+        public AuthorAttribute(string authorName) 
+        {
+        }
+    }
+}
